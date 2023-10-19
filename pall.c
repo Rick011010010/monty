b7efc3ae6a6f3797873e1602432a6f8dda63b3b8
elmyar ossama <oussama.elmyar92@gmail.com>
2023-10-19 11:54:15 -0700

    lifo

---
+++ pall.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * pull_fnc - prints the stack
+ * @head: stack head
+ * @counter: no used
+ * Return: no return
+*/
+void pull_fnc(stack_t **head, unsigned int counter)
+{
+	stack_t *he;
+	(void)counter;
+
+	he = *head;
+	if (he == NULL)
+		return;
+	while (he)
+	{
+		printf("%d\n", he->n);
+		he = he->next;
+	}
+}
